eb7466f5b50c72d5cc0f90a4f9b6708129c8198f
Ronak Shah <ronak.shah@outlook.com>
2023-05-07 11:52:38 -0700

    Added a story output chain to the name object

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 import { OpenAI } from "langchain/llms/openai";
 import { PromptTemplate } from "langchain/prompts";
-import { LLMChain } from "langchain/chains";
+import { SimpleSequentialChain, LLMChain } from "langchain/chains";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -38,10 +38,13 @@ export default async function (req, res) {
     //   temperature: 0.6,
     // });
 
-    const model = new OpenAI({model_name : "text-davinci-003", temperature : 0.9});
-    const chain = new LLMChain({ llm: model, prompt: generatePromptTemplate() });
-    const llmResponse = await chain.call({ animal: capitalizedAnimal });
-    res.status(200).json({ result: llmResponse.text });
+    const nameModel = new OpenAI({model_name : "text-davinci-003", temperature : 0.9});
+    const nameChain = new LLMChain({ llm: nameModel, prompt: generateNameTemplate() });
+    const storyModel = new OpenAI({model_name : "text-davinci-003", temperature : 0.9});
+    const storyChain = new LLMChain({ llm: storyModel, prompt: generateStoryTemplate() });
+    const overallChain = new SimpleSequentialChain({ chains: [nameChain, storyChain], verbose: true, });
+    const chainResponse = await overallChain.run(capitalizedAnimal);
+    res.status(200).json({result : chainResponse});
     // res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
@@ -59,7 +62,7 @@ export default async function (req, res) {
   }
 }
 
-function generatePromptTemplate() {
+function generateNameTemplate() {
   const animalNameTemplate = `Suggest three names for an animal that is a superhero.
 
 Animal: Cat
@@ -72,6 +75,20 @@ Names:`;
   const prompt = new PromptTemplate({
     template : animalNameTemplate,
     inputVariables: ["animal"],
+  });
+
+  return prompt;
+}
+
+function generateStoryTemplate() {
+  const storyTemplate = `You are a fiction author. Write a story about these superhero animals and their adventures to solve world peace. 
+  
+  Superhero animal names: 
+  {names}`
+
+  const prompt = new PromptTemplate({
+    template : storyTemplate,
+    inputVariables: ["names"],
   });
 
   return prompt;
